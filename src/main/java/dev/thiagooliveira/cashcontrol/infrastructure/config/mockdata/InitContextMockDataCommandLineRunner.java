f10ebfa3484f2bef13ce06916078babe03d800b8
Thiago Oliveira <thiago@thiagoti.com>
2025-12-12 22:47:00 +0000

    fix bug

--- src/main/java/dev/thiagooliveira/cashcontrol/infrastructure/config/mockdata/InitContextMockDataCommandLineRunner.java
+++ src/main/java/dev/thiagooliveira/cashcontrol/infrastructure/config/mockdata/InitContextMockDataCommandLineRunner.java
@@ -24,6 +24,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 
 @Component
 @Profile("mock-data")
@@ -68,6 +69,7 @@ public class InitContextMockDataCommandLineRunner implements CommandLineRunner {
   }
 
   @Override
+  @Transactional
   public void run(String... args) throws Exception {
     var user = userJpaRepository.findAll().stream().findFirst();
     if (user.isPresent()) {
