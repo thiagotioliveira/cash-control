cc07de5d7d3e460182eb5efac7d3614c815eb3c6
Thiago Oliveira <thiago@thiagoti.com>
2025-12-19 17:46:25 +0000

    * fix bug

--- src/main/java/dev/thiagooliveira/cashcontrol/infrastructure/web/manager/model/AccountModel.java
+++ src/main/java/dev/thiagooliveira/cashcontrol/infrastructure/web/manager/model/AccountModel.java
@@ -5,14 +5,17 @@ import static dev.thiagooliveira.cashcontrol.infrastructure.web.manager.Formatte
 import dev.thiagooliveira.cashcontrol.domain.account.AccountSummary;
 import java.time.Instant;
 import java.time.LocalDateTime;
+import java.util.UUID;
 
 public class AccountModel {
 
+  private final UUID id;
   private final Instant updatedAt;
   private final String name;
   private final String balance;
 
   public AccountModel(AccountSummary account) {
+    this.id = account.id();
     this.updatedAt = account.updatedAt();
     this.name = account.name();
     var symbol = account.currency().getSymbol();
@@ -29,5 +32,9 @@ public class AccountModel {
 
   public String getBalance() {
     return balance;
+  }
+
+  public UUID getId() {
+    return id;
   }
 }
